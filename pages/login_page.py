013bd17966a6462d5356ea154328350be2ba24e6
Raffael Isalberti <156975953+RaffaelIsalberti@users.noreply.github.com>
2024-04-18 09:49:05 -0300

    Add files via upload

---
+++ pages/login_page.py
@@ -0,0 +1,20 @@
+from selenium.webdriver.common.by import By
+import conftest
+from pages.base_page import BasePage
+
+
+class LoginPage(BasePage):
+
+    def __init__(self):
+        self.driver = conftest.driver
+        self.field_users = (By.XPATH, "//a[@class='ico-login']")
+        self.username_field = (By.XPATH, "//input[@class='email']")
+        self.password_field = (By.XPATH, "//input[@class='password']")
+        self.login_button = (By.XPATH, "//button[text()= 'Log in']")
+
+    ##LOGIN -- melhorar com base page
+    def fazer_login(self, usuario, senha):
+        self.cliq(self.field_users)
+        self.writer(self.username_field, usuario)
+        self.writer(self.password_field, senha)
+        self.cliq(self.login_button)
